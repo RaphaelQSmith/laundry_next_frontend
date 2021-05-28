f664f88b409f5ead57118bb738262d62471916d5
RaphaelQSmith <49982690+RaphaelQSmith@users.noreply.github.com>
2021-05-27 21:37:10 -0300

    Working with Date and fixing Table data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import styles from '../components/components.module.scss'
+import Loginform from './roster/loginform'
 
 export default function index() {
     return (
         <div className={styles.homeContainer}>
-            <h2>Welcome to the Online Laundry Roster System</h2>            
+            <h2>Welcome to the Online Laundry Roster System</h2>
+            <Loginform />            
         </div>
     )
 }
--- src/pages/roster/table.tsx
+++ src/pages/roster/table.tsx
@@ -1,42 +1,47 @@
 import styles from '../../components/components.module.scss'
 
+var time = new Date()
+var today = time.getFullYear() + "/" + (time.getMonth()+1) + "/" + time.getDate() 
 export default function table() {
     return (
-        <div className={styles.tableHead}>
-            <table>
-                <thead>
-                    <th></th>
-                    <th>Friday</th>
-                    <th>Saturday</th>
-                    <th>Sunday</th>
-                    <th>Monday</th>
-                    <th>Tuesday</th>
-                    <th>Wednesday</th>
-                    <th>Thursday</th>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Morning</td>
-                        <td>Daniele</td>
-                        <td>Daniele</td>
-                        <td>Daniele</td>
-                        <td>Daniele</td>
-                        <td>Daniele</td>
-                        <td>Daniele</td>
-                        <td>Daniele</td>
-                    </tr>
-                    <tr>
-                        <td>Evening</td>
-                        <td>Francisca</td>
-                        <td>Francisca</td>
-                        <td>Francisca</td>
-                        <td>Francisca</td>
-                        <td>Francisca</td>
-                        <td>Francisca</td>
-                        <td>Francisca</td>
-                    </tr>
-                </tbody>
-            </table>
+        <div>
+            <h3>{today}</h3>
+            <div className={styles.tableHead}>
+                <table>
+                    <thead>
+                        <th></th>
+                        <th>Friday</th>
+                        <th>Saturday</th>
+                        <th>Sunday</th>
+                        <th>Monday</th>
+                        <th>Tuesday</th>
+                        <th>Wednesday</th>
+                        <th>Thursday</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Morning</td>
+                            <td>Daniele</td>
+                            <td>Daniele</td>
+                            <td>Daniele</td>
+                            <td>Daniele</td>
+                            <td>Daniele</td>
+                            <td>Daniele</td>
+                            <td>Daniele</td>
+                        </tr>
+                        <tr>
+                            <td>Evening</td>
+                            <td>Francisca</td>
+                            <td>Francisca</td>
+                            <td>Francisca</td>
+                            <td>Francisca</td>
+                            <td>Francisca</td>
+                            <td>Francisca</td>
+                            <td>Francisca</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
